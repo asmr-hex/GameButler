736c6180130b3d3b3e448fd5378ae3ccfe0f0a6d
connorwalsh <c@polygon.pizza>
2016-07-21 23:37:51 -0400

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # GameButler
 I live to serve games.
+
+
+### Install
+```shell
+$ npm install gamebutler
+```
+
+### Usage
+```shell
+$node node_modules/
+```
---
+++ src/gamebutler.js
@@ -0,0 +1,62 @@
+import http from 'http'
+import path from 'path'
+import url from 'url'
+import fs from 'fs'
+
+// check for arguments
+if (process.argv.length < 3) {
+    // user must specify entry html page
+    console.log(`\nUsage: \n\t$ node gamebutler.js path/to/index.html\n`)
+    process.exit()
+}
+
+// store const entrypoint
+const ENTRY = path.resolve(process.argv[2])
+const BASE  = path.dirname(ENTRY)
+const PORT  = 8080
+
+// create server
+let server = http.createServer((request, response) => {
+    let uri = url.parse(request.url).pathname
+    let filename = path.join(BASE, uri)
+    fs.stat(filename, (err, status) => {
+	if (err != null) {
+	    response.writeHead(200, {'Content-Type': 'text/plain'})
+            response.write('404')
+	    response.end()
+	} else {
+	    // if we are on the root directory, serve entrypoint
+	    if (status.isDirectory()) {
+		filename = ENTRY
+	    }
+	    // get file extension
+	    let ext = path.extname(filename).split(".")[1]
+	    let mimeType = ""
+	    switch (ext) {
+		case "html":
+		mimeType = "text/html"
+		break
+		case "jpg":
+		case "jpeg":
+		mimeType = "image/jpeg"
+		break
+		case "png":
+		mimeType ="image/png"
+		break
+		case "js":
+		mimeType ="text/javascript"
+		break
+		case "css":
+		mimeType ="text/css"
+		break	
+	    }
+	    response.writeHead(200, {'Content-Type': mimeType});
+	    let fileStream = fs.createReadStream(filename)
+            fileStream.pipe(response)
+	}
+    })
+}).listen(PORT, () => {
+    console.log("GameButler listening on: http://localhost:%s", PORT)
+})
+
+
